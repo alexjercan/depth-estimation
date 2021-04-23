f9f7ab7238ec8746db24a96ec2dd215beade6af6
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-04-23 19:45:26 +0300

    test

--- model.py
+++ model.py
@@ -126,9 +126,9 @@ class DisparityRefinment(nn.Module):
         self.conv =  nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
-        r = F.interpolate(x, scale_factor=self.scale, mode='bilinear', align_corners=False)
-        x = self.residual_blocks(r)
-        return r + self.conv(x)
+        x = F.interpolate(x, scale_factor=self.scale, mode='bilinear', align_corners=False)
+        x = self.residual_blocks(x)
+        return self.conv(x)
 
 
 class Model(nn.Module):
