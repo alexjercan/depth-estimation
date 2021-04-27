8d031859af915d1f87d8366fdc7e186ce5996e24
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-04-27 19:23:13 +0300

    pls

--- metrics.py
+++ metrics.py
@@ -31,24 +31,24 @@ def evaluate_error(pred_depth, gt_depth):
              }
     _pred_depth = pred_depth[depth_mask]
     _gt_depth   = gt_depth[depth_mask]
-    n_valid_element = _gt_depth.size(0)
+    n_valid_element = float(_gt_depth.size(0))
 
     if n_valid_element > 0:
         diff_mat = torch.abs(_gt_depth-_pred_depth)
         rel_mat = torch.div(diff_mat, _gt_depth)
-        error['MSE'] = torch.sum(torch.pow(diff_mat, 2))/n_valid_element
+        error['MSE'] = torch.div(torch.sum(torch.pow(diff_mat, 2)), n_valid_element)
         error['RMSE'] = torch.sqrt(error['MSE'])
-        error['MAE'] = torch.sum(diff_mat)/n_valid_element
-        error['ABS_REL'] = torch.sum(rel_mat)/n_valid_element
+        error['MAE'] = torch.div(torch.sum(diff_mat), n_valid_element)
+        error['ABS_REL'] = torch.div(torch.sum(rel_mat), n_valid_element)
         y_over_z = torch.div(_gt_depth, _pred_depth)
         z_over_y = torch.div(_pred_depth, _gt_depth)
         max_ratio = max_of_two(y_over_z, z_over_y)
-        error['DELTA1.02'] = torch.sum(max_ratio < 1.02).numpy()/float(n_valid_element)
-        error['DELTA1.05'] = torch.sum(max_ratio < 1.05).numpy()/float(n_valid_element)
-        error['DELTA1.10'] = torch.sum(max_ratio < 1.10).numpy()/float(n_valid_element)
-        error['DELTA1.25'] = torch.sum(max_ratio < 1.25).numpy()/float(n_valid_element)
-        error['DELTA1.25^2'] = torch.sum(max_ratio < 1.25**2).numpy()/float(n_valid_element)
-        error['DELTA1.25^3'] = torch.sum(max_ratio < 1.25**3).numpy()/float(n_valid_element)
+        error['DELTA1.02'] = torch.div(torch.sum(max_ratio < 1.02), n_valid_element)
+        error['DELTA1.05'] = torch.div(torch.sum(max_ratio < 1.05), n_valid_element)
+        error['DELTA1.10'] = torch.div(torch.sum(max_ratio < 1.10), n_valid_element)
+        error['DELTA1.25'] = torch.div(torch.sum(max_ratio < 1.25), n_valid_element)
+        error['DELTA1.25^2'] = torch.div(torch.sum(max_ratio < 1.25**2), n_valid_element)
+        error['DELTA1.25^3'] = torch.div(torch.sum(max_ratio < 1.25**3), n_valid_element)
     return error
 
 
@@ -58,8 +58,8 @@ def avg_error(error_sum, error_step, total_step, batch_size):
                  'DELTA1.02':0, 'DELTA1.05':0, 'DELTA1.10':0, \
                  'DELTA1.25':0, 'DELTA1.25^2':0, 'DELTA1.25^3':0,}
     for item, value in error_step.items():
-        error_sum[item] += error_step[item] * batch_size
-        error_avg[item] = error_sum[item]/float(total_step)
+        error_sum[item] += value.item() * batch_size
+        error_avg[item] = error_sum[item] / float(total_step)
     return error_avg
 
 
