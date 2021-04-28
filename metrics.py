b9585dcf240038204e4167b3d4478b9ac030e8bb
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-04-28 21:18:40 +0300

    added normal metrics

--- metrics.py
+++ metrics.py
@@ -4,8 +4,10 @@
 #
 # References:
 # - https://github.com/XinJCheng/CSPN/blob/b3e487bdcdcd8a63333656e69b3268698e543181/cspn_pytorch/utils.py#L19
+# - https://web.eecs.umich.edu/~fouhey/2016/evalSN/evalSN.html
 #
 
+from math import radians
 import torch
 
 
@@ -20,7 +22,7 @@ class MetricFunction():
         (depth_p, normal_p) = predictions
         (depth_gt, normal_gt) = targets
         
-        error_val = evaluate_error(depth_p, depth_gt)
+        error_val = dict(evaluate_error_depth(depth_p, depth_gt), **evaluate_error_normal(normal_p, normal_gt))
         
         self.total_size += self.batch_size
         self.error_avg = avg_error(self.error_sum, error_val, self.total_size, self.batch_size)
@@ -28,13 +30,15 @@ class MetricFunction():
     
     def show(self):
         error = self.error_avg
-        format_str = ('MSE=%.4f\tRMSE=%.4f\tMAE=%.4f\tABS_REL=%.4f\nDELTA1.02=%.4f\tDELTA1.05=%.4f\tDELTA1.10=%.4f\nDELTA1.25=%.4f\tDELTA1.25^2=%.4f\tDELTA1.25^3=%.4f')
-        return format_str % (error['MSE'], error['RMSE'], error['MAE'],  error['ABS_REL'], \
-                         error['DELTA1.02'], error['DELTA1.05'], error['DELTA1.10'], \
-                         error['DELTA1.25'], error['DELTA1.25^2'], error['DELTA1.25^3'])
-   
+        format_str = ('======DEPTH=======\nMSE=%.4f\tRMSE=%.4f\tMAE=%.4f\tABS_REL=%.4f\nDELTA1.02=%.4f\tDELTA1.05=%.4f\tDELTA1.10=%.4f\nDELTA1.25=%.4f\tDELTA1.25^2=%.4f\tDELTA1.25^3=%.4f\n======NORMALS=======\nMSE=%.4f\tRMSE=%.4f\tMAE=%.4f\tMME=%.4f\nTANGLE11.25=%.4f\tTANGLE22.5=%.4f\tTANGLE30.0=%.4f')
+        return format_str % (error['D_MSE'], error['D_RMSE'], error['D_MAE'],  error['D_ABS_REL'], \
+                         error['D_DELTA1.02'], error['D_DELTA1.05'], error['D_DELTA1.10'], \
+                         error['D_DELTA1.25'], error['D_DELTA1.25^2'], error['D_DELTA1.25^3'], \
+                         error['N_MSE'], error['N_RMSE'], error['N_MAE'],  error['N_MME'], \
+                         error['N_TANGLE11.25'], error['N_TANGLE22.5'], error['N_TANGLE30.0'])
 
-def evaluate_error(pred_depth, gt_depth):
+
+def evaluate_error_depth(pred_depth, gt_depth):
     # for numerical stability
     depth_mask = gt_depth>0.0001
     batch_size = gt_depth.size(0)
@@ -47,19 +51,38 @@ def evaluate_error(pred_depth, gt_depth):
         n_valid_element = torch.tensor(n_valid_element, device=pred_depth.device)
         diff_mat = torch.abs(_gt_depth-_pred_depth)
         rel_mat = torch.div(diff_mat, _gt_depth)
-        error['MSE'] = torch.div(torch.sum(torch.pow(diff_mat, 2)), n_valid_element)
-        error['RMSE'] = torch.sqrt(error['MSE'])
-        error['MAE'] = torch.div(torch.sum(diff_mat), n_valid_element)
-        error['ABS_REL'] = torch.div(torch.sum(rel_mat), n_valid_element)
+        error['D_MSE'] = torch.div(torch.sum(torch.pow(diff_mat, 2)), n_valid_element)
+        error['D_RMSE'] = torch.sqrt(error['D_MSE'])
+        error['D_MAE'] = torch.div(torch.sum(diff_mat), n_valid_element)
+        error['D_ABS_REL'] = torch.div(torch.sum(rel_mat), n_valid_element)
         y_over_z = torch.div(_gt_depth, _pred_depth)
         z_over_y = torch.div(_pred_depth, _gt_depth)
         max_ratio = torch.max(y_over_z, z_over_y)
-        error['DELTA1.02'] = torch.div(torch.sum(max_ratio < 1.02), n_valid_element)
-        error['DELTA1.05'] = torch.div(torch.sum(max_ratio < 1.05), n_valid_element)
-        error['DELTA1.10'] = torch.div(torch.sum(max_ratio < 1.10), n_valid_element)
-        error['DELTA1.25'] = torch.div(torch.sum(max_ratio < 1.25), n_valid_element)
-        error['DELTA1.25^2'] = torch.div(torch.sum(max_ratio < 1.25**2), n_valid_element)
-        error['DELTA1.25^3'] = torch.div(torch.sum(max_ratio < 1.25**3), n_valid_element)
+        error['D_DELTA1.02'] = torch.div(torch.sum(max_ratio < 1.02), n_valid_element)
+        error['D_DELTA1.05'] = torch.div(torch.sum(max_ratio < 1.05), n_valid_element)
+        error['D_DELTA1.10'] = torch.div(torch.sum(max_ratio < 1.10), n_valid_element)
+        error['D_DELTA1.25'] = torch.div(torch.sum(max_ratio < 1.25), n_valid_element)
+        error['D_DELTA1.25^2'] = torch.div(torch.sum(max_ratio < 1.25**2), n_valid_element)
+        error['D_DELTA1.25^3'] = torch.div(torch.sum(max_ratio < 1.25**3), n_valid_element)
+    return error
+
+
+def evaluate_error_normal(pred_normal, gt_normal):
+    error = {}
+    
+    dot_product = torch.mul(pred_normal, gt_normal).sum(dim=1)
+    angular_error = torch.acos(torch.minimum(torch.tensor(1, device=pred_normal.device), 
+                                             torch.maximum(torch.tensor(-1, device=pred_normal.device), dot_product)))
+
+    error['N_MSE'] = torch.mean(torch.mul(angular_error, angular_error))
+    error['N_RMSE'] = torch.sqrt(error['N_MSE'])
+    error['N_MAE'] = torch.mean(angular_error)
+    error['N_MME'] = torch.median(angular_error)
+    
+    error['N_TANGLE11.25'] = torch.mean((angular_error < radians(11.25)).float())
+    error['N_TANGLE22.5'] = torch.mean((angular_error < radians(22.5)).float())
+    error['N_TANGLE30.0'] = torch.mean((angular_error < radians(30.0)).float())
+    
     return error
 
 
