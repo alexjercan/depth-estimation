e1affd155366476de0b61732eafa05517ce3e8a1
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-04-29 12:52:41 +0300

    fixed random A bug

--- dataset.py
+++ dataset.py
@@ -135,8 +135,8 @@ if __name__ == "__main__":
             M.MyVerticalFlip(p=0.1),
             A.OneOf([
                 A.MotionBlur(p=0.2),
-                A.MedianBlur(p=0.1),
-                A.Blur(p=0.1),
+                A.MedianBlur(blur_limit=3, p=0.1),
+                A.Blur(blur_limit=3, p=0.1),
             ], p=0.2),
             A.OneOf([
                 M.MyOpticalDistortion(p=0.3),
